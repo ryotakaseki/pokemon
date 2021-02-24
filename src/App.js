50efd1df0107dcd8664c48cd67e5fda18c452dc6
Kenichi Okiebisu <a01056715@gmail.com>
2021-02-23 20:54:21 -0800

    内容

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import axios from 'axios';
+import React from 'react';
 
 class App extends React.Component {
   state = {
-    inputValue: "",
+    inputValue: '',
+    error: '',
+    data: null,
   };
 
   // class型のコンポーネントの時はconstはいらない
@@ -12,18 +15,53 @@ class App extends React.Component {
     });
   };
 
-  buttonChange = () => {
-    alert(this.state.inputValue);
+  search = async () => {
+    try {
+      const search =
+        'https://pokeapi.co/api/v2/pokemon/' + this.state.inputValue;
+      const response = await axios.get(search);
+      const data = response.data;
+      this.setState({
+        data: data,
+      });
+      this.setState({ error: '' });
+    } catch (err) {
+      this.setState({ error: 'ポケモンの名前が間違っています！' });
+      this.setState({
+        data: null,
+      });
+    }
   };
 
   render() {
     return (
-      <div>
-        <input
-          value={this.state.inputValue}
-          onChange={(event) => this.inputChange(event)}
-        />
-        <button onClick={this.buttonChange}>検索</button>
+      <div class="flex flex-col items-center">
+        <div class="font-bold text-2xl text-gray-500 my-12">
+          <h3>ポケモン図鑑</h3>
+        </div>
+        <div class="my-8 w-96">
+          <div class="bg-blue-500 w-full">
+            <input
+              class="text-center w-full text-3xl p-3 border-b border-gray-100 outline-none font-sans text-gray-700"
+              value={this.state.inputValue}
+              onChange={(event) => this.inputChange(event)}
+            />
+          </div>
+          <div class="my-8 flex justify-center">
+            <button
+              class="mx-auto rounded-lg px-24 py-4 inline-block bg-red-500 text-white font-bold"
+              onClick={this.search}
+            >
+              検索
+            </button>
+          </div>
+          <div>
+            <h2 class="text-center text-red-500 text-xl">{this.state.error}</h2>
+          </div>
+        </div>
+        <div>
+          {this.state.data ? <div>体重：{this.state.data.weight}</div> : null}
+        </div>
       </div>
     );
   }
